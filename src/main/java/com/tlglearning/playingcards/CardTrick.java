d6dbc8e67e971b07f64d481bb6d0db393982de3a
ngonpaul <pmgonzalez05@outlook.com>
2022-06-22 09:47:24 -0700

    Made comparator a local class in tally method.

--- src/main/java/com/tlglearning/playingcards/CardTrick.java
+++ src/main/java/com/tlglearning/playingcards/CardTrick.java
@@ -67,6 +67,19 @@ public CardTrick(){
                 redCount++;
             }
         }
+
+        class DisplayComparator implements Comparator<Card> {
+
+            @Override
+            public int compare(Card card1, Card card2) {
+                int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());
+                comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
+                comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
+                return comparison;
+            }
+
+        }
+
         Comparator<Card> comparator = new DisplayComparator();
         Collections.sort((LinkedList<Card>) blackPile, comparator);
         Collections.sort((LinkedList<Card>) redPile, comparator);
@@ -74,16 +87,4 @@ public CardTrick(){
         System.out.printf("Red: count%d, cards%s%n", redCount, redPile);
     }
 
-}
-
-
-class DisplayComparator implements Comparator<Card> {
-
-    @Override
-    public int compare(Card card1, Card card2) {
-        int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());
-        comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
-        comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
-        return comparison;
-    }
 }
